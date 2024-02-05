6de07c4dadce3e52fd4edbbb86997fbd97007710
Peter Grant Hoffman <phoffm10@hydra21.eecs.utk.edu>
2024-02-05 09:58:08 -0500

    Planning lab3

--- Lab3/Lab-3-Huffman/huff_dec.c
+++ Lab3/Lab-3-Huffman/huff_dec.c
@@ -1,2 +1,13 @@
 //Peter Hoffman
 //CS360 Lab3
+
+//need to parse code definition file
+//store values as decoding key
+//parse input file to stardard output
+
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
