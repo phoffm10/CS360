90be358ed146fb71d6484290b4823f1528baf469
Peter Grant Hoffman <phoffm10@hydra21.eecs.utk.edu>
2024-02-09 23:55:19 -0500

    needs more debugging

--- Lab2/Lab-2-Famtree/src/famtree.c
+++ Lab2/Lab-2-Famtree/src/famtree.c
@@ -38,6 +38,7 @@ void print_person(Person* p, JRB tree, JRB tmp){
    printf("-----------------------\n");
     jrb_traverse(tmp, tree) {
         p = (Person *) tmp->val.v;
+        //p->printed = true;
         printf("%s\n", p->name);
         printf("  Sex: %s\n", p->sex ? p->sex : "Unknown");
         //printf("  Father: %s\n",p->father->name);
@@ -60,6 +61,9 @@ void print_person(Person* p, JRB tree, JRB tmp){
 
         printf("    %s\n", p->children[j]->name);
     }
+    printf("num parents: %d\n", p->num_parents);
+    printf("visited: %d\n", p->visited);
+    printf("printed: %d\n", p->printed);
     printf("\n"); 
     }
     printf("-----------------------\n");
@@ -100,7 +104,10 @@ Person* get_person(Person* p, JRB tree, char* name){
         p->father = NULL;
         p->sex = NULL;
         p->num_children = 0;
-        p->max_children = 10;
+        p->max_children = 20;
+        p->num_parents = 0;
+        p->visited = false;
+        p->printed = false;
         p->children = malloc(p->max_children * sizeof(Person*));
         jrb_insert_str(tree, p->key, new_jval_v((void *) p));
         return p;
@@ -111,10 +118,67 @@ Person* get_person(Person* p, JRB tree, char* name){
     }
 }
 
+void print(Person* p){
+    printf("%s\n", p->name);
+    printf("  Sex: %s\n", p->sex ? p->sex : "Unknown");
+    //printf("  Father: %s\n",p->father->name);
+    printf("  Father: %s\n", p->father!=NULL ? p->father->name : "Unknown");
+    printf("  Mother: %s\n", p->mother!=NULL ? p->mother->name : "Unknown");
+    printf("  Children: ");
+    if(p->num_children == 0){
+    printf("None\n");
+    }
+        //printf("%d %p\n", p->num_children, p->children);
+        //fflush(stdout);
+    for (int j = 0; j < p->num_children; j++) {       
+        if(j == 0){
+            printf("\n");
+        }
+        if(j != 0 && !strcmp(p->children[j]->name,p->children[j-1]->name)){
+            continue;
+        }
+        printf("    %s\n", p->children[j]->name);
+    }
+    printf("\n");
+}
 //searches for key and reinserts person with same key
 void update_node(JRB tree, char* name, Person* p){
     jrb_delete_node(jrb_find_str(tree, name));
     jrb_insert_str(tree, name, new_jval_v((void *) p));
+}
+
+bool all_printed(JRB tree){
+    JRB tmp;
+    jrb_traverse(tmp, tree){
+        Person* p = (Person *)tmp->val.v;
+        if (p->printed == false){
+            return false;
+        }
+    }
+    return true;
+}
+
+void topsort(JRB tree){
+    JRB tmp;
+    while(!all_printed(tree)){
+        jrb_traverse(tmp, tree){
+            Person* p = (Person*)tmp->val.v;
+            if(!p->printed && (p->num_parents == 0)){
+                print(p);
+                p->printed = true;
+                for (int j = 0; j < p->num_children; j++) {      
+                    p->children[j]->num_parents--;
+                }
+            }
+        }
+        jrb_traverse(tmp, tree){
+            Person* p = (Person*)tmp->val.v;
+            if(!p->printed){
+                print(p);
+                p->printed = true;
+            }
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -179,7 +243,27 @@ int main(int argc, char *argv[]) {
             
             //checks if father already exists
             p->father = get_person(p->father, tree, p->father->name);
-            
+            p = get_person(p, tree, p->name);
+
+            /*
+            if(p->father->num_children == 0){
+                int children = p->father->num_children;
+                for(int j = 0; j < children; j++){
+                    if (strcmp(p->name, p->father->children[j]->name) != 0){
+                        p->father->children[p->father->num_children] = malloc(sizeof(Person));
+                        p->father->children[p->father->num_children]->name = strdup(p->name);
+                        p->father->num_children++;
+                        p->num_parents++;
+                    }
+                }
+            }
+            else{
+                p->father->children[p->father->num_children] = malloc(sizeof(Person));
+                p->father->children[p->father->num_children]->name = strdup(p->name);
+                p->father->num_children++;
+                p->num_parents++;
+            }
+            */
             //father duplicate error check
 
             //increments childs mother's number of children and adds childs name
@@ -190,8 +274,12 @@ int main(int argc, char *argv[]) {
             //sets fathers sex as male
             p->father->sex = strdup("Male");
 
+            //adds parent to num parents
+            p->num_parents++;
+
             //updates fathers node in tree
             update_node(tree, p->father->name, p->father);
+            update_node(tree, p->name, p);
         }
         //stores mother of person
         else if (strcmp(temp, "MOTHER") == 0) {
@@ -208,17 +296,41 @@ int main(int argc, char *argv[]) {
             
             //check of mother already exists
             p->mother = get_person(p->mother, tree, p->mother->name);
+            p = get_person(p, tree, p->name);
+
+            /*
+            if(p->mother->num_children == 0){
+                int children = p->mother->num_children;
+                for(int j = 0; j < children; j++){
+                    if (strcmp(p->name, p->mother->children[j]->name) != 0){
+                        p->mother->children[p->mother->num_children] = malloc(sizeof(Person));
+                        p->mother->children[p->mother->num_children]->name = strdup(p->name);
+                        p->mother->num_children++;
+                        p->num_parents++;
+                    }
+                }
+            }
+            else{
+                p->mother->children[p->mother->num_children] = malloc(sizeof(Person));
+                p->mother->children[p->mother->num_children]->name = strdup(p->name);
+                p->mother->num_children++;
+                p->num_parents++;
+            }
+            */
 
             //increments childs mother's number of children and adds childs name
             p->mother->children[p->mother->num_children] = malloc(sizeof(Person));
             p->mother->children[p->mother->num_children]->name = strdup(p->name);
             p->mother->num_children++;
-        
+            p->num_parents++;
+
+
             //sets mothers sex as female            
             p->mother->sex = strdup("Female");
-            
+
             //updates mothers node in tree
             update_node(tree, p->mother->name, p->mother);
+            update_node(tree, p->name, p);
 
         }
         //stores father of in person 
@@ -247,12 +359,19 @@ int main(int argc, char *argv[]) {
             //check if child already exists
             p->children[p->num_children] = get_person(p->children[p->num_children], tree, p->children[p->num_children]->name);
             
+            //printf("@@@@@@@@@@@@@\n");
+            //printf("p->name: %s\n", p->name);
+            //printf("p->children[p->num_children]->name: %s\n", p->children[p->num_children]->name);
+            //printf("@@@@@@@@@@@@@\n");
+
             //sets childs father as current person
             p->children[p->num_children]->father = p;
-
+            p->children[p->num_children]->num_parents++;
+            p->children[p->num_children] = get_person(p->children[p->num_children], tree, p->children[p->num_children]->name);
             //updates child node within father
             update_node(tree, p->children[p->num_children]->name, p->children[p->num_children]);
-
+            update_node(tree, p->name, p);
+            //printf("$$$$$ num parents: %d", p->children[p->num_children]->num_parents);
             p->num_children++;
         }
         //stores mother of in person 
@@ -268,6 +387,7 @@ int main(int argc, char *argv[]) {
             }
 
             //allocates mem for child and reads name
+            //Person *child = malloc(sizeof(Person));
             p->children[p->num_children] = malloc(sizeof(Person));
             strcpy(name, is->fields[1]);
             for (i = 1; i < is->NF-1; i++) {
@@ -277,13 +397,43 @@ int main(int argc, char *argv[]) {
             
             //stores read name in child of father
             p->children[p->num_children]->name = strdup(name);
-            
-            //check if child already exists
-            p->children[p->num_children] = get_person(p->children[p->num_children], tree, p->children[p->num_children]->name);
+            //child->name = strdup(name);
 
+            //check if child already exists
+            //child = get_person(child, tree, child->name);
+            //p->children[p->num_children] = get_person(p->children[p->num_children], tree, child->name);
+
+            p->children[p->num_children] = get_person(p->children[p->num_children], tree, p->children[p->num_children]->name);
+            //printf("@@@@@@@@@@@@@\n");
+            //printf("name: %s",name);
+            //printf("p->name: %s\n", p->name);
+            //printf("p->children[p->num_children]->name: %s\n", p->children[p->num_children]->name);
+            //printf("num_children: %d", p->num_children);
+            //printf("@@@@@@@@@@@@@\n");
+/*
+//--------
+            bool childExists = false;
+            for (int j = 0; j < p->num_children; j++) {
+                if (strcmp(p->children[j]->name, child->name) == 0) {
+                childExists = true;
+                break;
+                }
+            }
+            if (!childExists) {
+            // set the child's father as the current person
+                child->mother = p;
+                child->num_parents++;
+
+            // add the child to the children array
+            p->children[p->num_children] = child;
+            p->num_children++;
+            }
+//----------
+*/            
             //sets mother of child as current person
             p->children[p->num_children]->mother = p;
-            
+            p->children[p->num_children]->num_parents++;
+            p->children[p->num_children] = get_person(p->children[p->num_children], tree, p->children[p->num_children]->name);
             //updates childs node within father
             update_node(tree, p->children[p->num_children]->name, p->children[p->num_children]);
 
@@ -306,7 +456,9 @@ int main(int argc, char *argv[]) {
       
     }
 
-    print_person(p, tree, tmp);
+    //print_person(p, tree, tmp);
+    //printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+    topsort(tree);
     //printf("%d", !jrb_empty(tree));
 
 /*
