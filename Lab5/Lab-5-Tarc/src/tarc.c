99989c267c0e4a33b13594f85fcd89cdb394502c
Peter Grant Hoffman <phoffm10@hydra21.eecs.utk.edu>
2024-03-10 14:10:06 -0400

    8/100

--- Lab5/Lab-5-Tarc/src/tarc.c
+++ Lab5/Lab-5-Tarc/src/tarc.c
@@ -36,17 +36,26 @@ int file_mode(char *filename);
 long file_mod_time(char *filename);
 char *file_bytes(char *filename);
 
-typedef struct filestruct{
-    char* path;
-    char* name;
-    char* bytes;
-    int name_size;
-    int mode;
-    long inode;
-    long mod_time;
-    long size;    
-    bool seen;
-} filestruct;
+char* dir_name(char* pathname){
+  //find index of final /
+  char* dirn;
+  int pathsize = strlen(pathname);
+  int index = 0;
+  for (int i = 0; i < pathsize; i++){
+    if(pathname[i] == '/'){
+      index = i + 1;
+    }
+  }
+  //copy from final / to end
+  if(index != 0){
+    dirn = (char*)malloc((pathsize-index)+1);
+    memcpy(dirn, pathname+index, pathsize-index);
+    return dirn;
+  }
+  else{
+    return pathname;
+  }
+}
 
 bool is_this_dir(char *filename)
 {
@@ -149,7 +158,7 @@ char *prefix(char *basedir, char *filename)
 {
   // malloc length of both + extra char
   char *path;
-  path = (char *)malloc((strlen(basedir) + strlen(filename) + 100) * sizeof(char));
+  path = (char *)malloc((strlen(basedir) + strlen(filename) + 150) * sizeof(char));
   strcpy(path, basedir);
   // strcpy base dir into ptr
   strcat(path, "/");
@@ -166,7 +175,8 @@ void* traverse(char* dir){
   struct dirent *de;
   struct stat fbuf;
   int exists;
-  char *file;
+  char* file;
+  char* displayname;
   bool seen = false;
 
   Dllist inodes;
@@ -181,11 +191,23 @@ void* traverse(char* dir){
     exit(1);
   }
 
+  char* nopathdir = dir_name(dir);
+/*
+  long example_inode;
+  fwrite(&example_inode, sizeof(example_inode), 1, stdout);
+
+  char *example_file_contents;
+  int file_size;
+
+  fwrite(example_file_contents, sizeof(char), file_size, stdout);
+*/
+
   for (de = readdir(d); de != NULL; de = readdir(d))
   {
     if(strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0) continue;
     // need to build name with prefix
     file = prefix(dir, de->d_name);
+    displayname = prefix(nopathdir, de->d_name);
     //printf("path: %s\n", file);
     seen = false;
 
@@ -196,43 +218,65 @@ void* traverse(char* dir){
     }
     else
     {
-      printf("Namesize: %d\n", (int)strlen(file));
-      printf("Name: %s\n", file);
-      printf("Inode: %ld\n", file_inode(file));
+      //int fnamesize = (int)strlen(file);
+      int fnamesize = (int)strlen(displayname);
+      //char* fname = file;
+      char* fname = displayname;
+      long finode = file_inode(file);
 
-      //inode check not working
+      fwrite(&fnamesize, sizeof(fnamesize), 1, stdout);
+      fwrite(fname, sizeof(char), fnamesize, stdout);
+      fwrite(&finode, sizeof(finode), 1, stdout);
+
+      // printf("Namesize: %d\n", (int)strlen(file));
+      // printf("Name: %s\n", file);
+      // printf("Inode: %ld\n", file_inode(file));
+
       dll_traverse(tmp, inodes){
         //if seen, set seen to true
         if(file_inode(file) == tmp->val.l){
-          printf("seen");
+          printf("\n");
           seen = true;
         }
-        //if not seen, add inode to list
-        else{
-          dll_append(inodes, new_jval_l(file_inode(file)));
-        }
       }
+      if (!seen) {
+        dll_append(inodes, new_jval_l(file_inode(file)));
+      }
+      /*
       printf("------------\n");
       dll_traverse(tmp, inodes){
         printf("%ld", tmp->val.l);
       }
       printf("------------\n");
-
+      */
       if(seen == false){
-        printf("Mode: %d\n", file_mode(file));
-        printf("Modification time: %ld\n", file_mod_time(file));
+        int fmode = file_mode(file);
+        long fmodt = file_mod_time(file);
+
+
+        fwrite(&fmode, sizeof(fmode), 1, stdout);
+        fwrite(&fmodt, sizeof(fmodt), 1, stdout);
+        // printf("Mode: %d\n", file_mode(file));
+        // printf("Modification time: %ld\n", file_mod_time(file));
         if (is_this_dir(file) == true )
         {
           //if its a directory, traverse directory
-          if(strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0) continue;
+          //if(strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0) continue;
           //printf("%s is a directory\n", file);
           printf("\n");
           traverse(file);
         }
         else
         {//is a file, use file logic to display things
-          printf("Size: %ld\n", file_size(file));
-          printf("Bytes: %s\n", file_bytes(file));
+          long fsize = file_size(file);
+          char* output = file_bytes(file);
+
+          fwrite(&fsize, sizeof(fsize), 1, stdout);
+          fwrite(output, sizeof(char), file_size(file), stdout);
+
+          // printf("Size: %ld\n", file_size(file));
+          // printf("Bytes: %s\n", file_bytes(file));
+          
           // printf("%s is not directory\n", file);
           // printf("%s namesize is: %d\n", file, strlen(file));
           // printf("%s size is: %d\n", file, file_size(file));
@@ -240,23 +284,49 @@ void* traverse(char* dir){
           // printf("%s mode is: %0x\n", file, file_mode(file));
           // printf("%s mod time is: %ld\n", file, file_mod_time(file));
           // printf("%s bytes are: %s\n", file, file_bytes(file));
+
         }
       }
     }
   }
+  closedir(d);
 }
 
 int main(int argc, char *argv[])
 {
-  //parent dir info
-  printf("Namesize: %d\n", (int)strlen(argv[1]));
-  printf("Name: %s\n", argv[1]);
-  printf("Inode: %ld\n", file_inode(argv[1]));
-  printf("Mode: %d\n", file_mode(argv[1]));
-  printf("Modification time: %ld\n", file_mod_time(argv[1]));
-  printf("\n");
+  //char* test = "/home/cs360/lab/lab5/directory";
+  //printf("test: %s", dir_name(test));
+  
+  
+  //need function to pull dir name only
+  char* pathname = strdup(argv[1]);
+  char* parentdir = NULL;
+  parentdir = dir_name(argv[1]);
+  //printf("pathname :%s\n",pathname);
+  //printf("parentdir :%s\n",parentdir);
 
-  traverse(argv[1]);
+  int fnamesize = (int)strlen(parentdir);
+  char* fname = parentdir;
+  long finode = file_inode(pathname);
+  int fmode = file_mode(pathname);
+  long fmodt = file_mod_time(pathname);
+
+  fwrite(&fnamesize, sizeof(fnamesize), 1, stdout);
+  fwrite(fname, sizeof(char), fnamesize, stdout);
+  fwrite(&finode, sizeof(finode), 1, stdout);
+  fwrite(&fmode, sizeof(fmode), 1, stdout);
+  fwrite(&fmodt, sizeof(fmodt), 1, stdout);
+
+
+
+  //printf("Namesize: %d\n", (int)strlen(parentdir));
+  // printf("Name: %s\n", argv[1]);
+  // printf("Inode: %ld\n", file_inode(argv[1]));
+  // printf("Mode: %d\n", file_mode(argv[1]));
+  // printf("Modification time: %ld\n", file_mod_time(argv[1]));
+  // printf("\n");
+
+  traverse(pathname);
 
   return 0;
 }
