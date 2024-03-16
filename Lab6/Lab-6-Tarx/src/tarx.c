4757dce83b01434f8b02896c1c23cd88b347ed30
Peter Grant Hoffman <phoffm10@hydra21.eecs.utk.edu>
2024-03-16 17:59:58 -0400

    first commit

---
+++ Lab6/Lab-6-Tarx/src/tarx.c
@@ -0,0 +1,18 @@
+//Peter Hoffman
+//Lab 6 CS360
+
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "jrb.h"
+#include "fields.h"
+#include "dllist.h"
+
+int main(int argc, char* argv[]){
+
+    return 0;
+}
